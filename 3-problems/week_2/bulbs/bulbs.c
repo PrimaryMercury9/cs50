2edcb85a109d74ca210c15035333bde336f09735
John <john.p.murphy04@gmail.com>
2023-07-31 12:17:28 +1000

    Initial commit, incomplete, migrating away from old big laptop

---
+++ 3-problems/week_2/bulbs/bulbs.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    // TODO
+    for (int i = 0; i < BITS_IN_BYTE; i++) {
+        print_bulb(binary[i]);
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
